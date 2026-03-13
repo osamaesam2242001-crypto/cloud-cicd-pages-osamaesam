9f7ade726f8f3852902b9787bf32cd145bf52a53
osamaesam2242001-crypto <osamaesam2242001@gmail.com>
2026-03-13 02:01:59 +0200

    Update script.js
    Replaced button click event with a function to show a success message.

--- final_project/script.js
+++ final_project/script.js
@@ -1,10 +1,4 @@
-const button = document.getElementById("btn");
-const message = document.getElementById("msg");
-
-button.addEventListener("click", () => {
-  const now = new Date();
-  const time = now.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
-
-  message.textContent = `Workflow completed successfully at ${time}. GitHub Pages is now updated.`;
-  button.textContent = "Deployment Completed";
-});
+function showMessage() {
+  document.getElementById("message").innerText =
+    "The website has been successfully updated through the CI/CD pipeline!";
+}
